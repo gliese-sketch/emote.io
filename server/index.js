715e9986537d1aec7afc4be500abccc0709513fd
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-02 11:37:44 +0000

    create server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log("Hello There!");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+server.listen("8000", () => {
+  console.log("Server listening on port 8000");
+});
